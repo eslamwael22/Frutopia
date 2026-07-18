5f98aea81fccea5c970f14b1c6a96ba730925cdd
Eslam Wael <eldasheslam105@gmail.com>
2026-07-18 13:07:55 +0300

    use Aseet generator extention

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,19 @@
+class Assets {
+  Assets._();
+
+  static const String assetsImagesAnanas = "assets/images/ananas.png";
+
+  static const String assetsImagesCircules = "assets/images/circules.svg";
+
+  static const String assetsImagesFruits = "assets/images/fruits.png";
+
+  static const String assetsImagesLogo = "assets/images/Logo.png";
+
+  static const String assetsImagesPatata = "assets/images/patata.svg";
+
+  static const String assetsImagesPlant = "assets/images/plant.svg";
+
+  static const String assetsImagesTrees = "assets/images/trees.png";
+
+  static const String assetsImagesVector = "assets/images/Vector.svg";
+}
